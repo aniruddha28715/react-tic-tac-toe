1931cc45048378ece3b14972e41464edbe2fb5f0
Aniruddha <103273966+aniruddha28715@users.noreply.github.com>
2024-11-09 09:53:01 +0530

    Initial commit

---
+++ 2024/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+import Board from "./TicTacToeGame/Board";
+
+
+function App() {
+  return (
+    <div className="App">
+      <Board />
+    </div>
+    
+  );
+}
+
+export default App;
